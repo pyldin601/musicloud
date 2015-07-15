f38a681a00178df411911c7d2ebea94de4a49cc8
pldin601 <roman@homefs.biz>
2015-07-15 23:51:05 +0300

    Initial commit

--- app/lang/Arrays.php
+++ app/lang/Arrays.php
@@ -13,4 +13,43 @@ class Arrays {
     public static function last(array $array) {
         return count($array) == 0 ? null : $array[count($array) - 1];
     }
+    public static function first(array $array) {
+        return count($array) == 0 ? null : $array[0];
+    }
+    public static function tail(array $array) {
+        return array_slice($array, 1);
+    }
+    public static function init(array $array) {
+        return array_slice($array, 0, count($array) - 1);
+    }
+    public static function drop($count, array $array) {
+        return array_slice($array, $count);
+    }
+    public static function dropRight($count, array $array) {
+        return array_slice($array, 0, count($array) - $count - 1);
+    }
+    public static function map($callback, array $array) {
+        foreach ($array as $item) {
+            yield $callback($item);
+        }
+    }
+    public static function filter($callback, array $array) {
+        foreach ($array as $item) {
+            if ($callback($item))
+                yield $item;
+        }
+    }
+    public static function reduce($callback, array $array) {
+        if (count($array) == 0) {
+            throw new \Exception("Empty array could not be reduced!");
+        } else if (count($array) == 1) {
+            return $array[0];
+        } else {
+            $temp = $callback($array[0], $array[1]);
+            foreach (self::drop(1, $array) as $item) {
+                $temp = $callback($temp, $item);
+            }
+            return $temp;
+        }
+    }
 }
