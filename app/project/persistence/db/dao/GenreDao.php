9ad061ee82cc0357204cfb5636e7d3477b221c00
pldin601 <roman@homefs.biz>
2015-07-23 20:56:56 +0300

    Added genre metadata data access object

--- app/project/persistence/db/dao/GenreDao.php
+++ app/project/persistence/db/dao/GenreDao.php
@@ -14,12 +14,24 @@ use app\core\db\builder\SelectQuery;
 use app\project\models\single\LoggedIn;
 use app\project\persistence\db\tables\MetaGenresTable;
 
+/**
+ * Class GenreDao
+ * @package app\project\persistence\db\dao
+ */
 class GenreDao {
     /** @var LoggedIn */
     private static $me;
     public static function class_init() {
         self::$me = resource(LoggedIn::class);
     }
+
+    /**
+     * Returns id of $genre from database if exists, or generates
+     * new row for $genre and returns generated id.
+     *
+     * @param $genre
+     * @return int
+     */
     public static function getGenreId($genre) {
         $genre_object = (new SelectQuery(MetaGenresTable::TABLE_NAME))
             ->where(MetaGenresTable::USER_ID, self::$me->getId())
