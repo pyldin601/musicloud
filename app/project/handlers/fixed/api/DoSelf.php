64156713f06a3236de2729377754147a00942257
pldin601 <roman@homefs.biz>
2015-07-27 12:20:20 +0300

    Initial frontend development

--- app/project/handlers/fixed/api/DoSelf.php
+++ app/project/handlers/fixed/api/DoSelf.php
@@ -9,16 +9,32 @@
 namespace app\project\handlers\fixed\api;
 
 
+use app\core\db\builder\SelectQuery;
 use app\core\router\RouteHandler;
 use app\core\view\JsonResponse;
 use app\project\models\single\LoggedIn;
+use app\project\persistence\db\tables\MetaAlbumsTable;
+use app\project\persistence\db\tables\MetaArtistsTable;
+use app\project\persistence\db\tables\MetadataTable;
+use app\project\persistence\db\tables\MetaGenresTable;
 
 class DoSelf implements RouteHandler {
     public function doGet(JsonResponse $response, LoggedIn $me) {
 
+        $artists = count(new SelectQuery(MetaArtistsTable::TABLE_NAME, MetaArtistsTable::USER_ID_FULL, $me->getId()));
+        $albums  = count(new SelectQuery(MetaAlbumsTable::TABLE_NAME,  MetaAlbumsTable::USER_ID_FULL,  $me->getId()));
+        $genres  = count(new SelectQuery(MetaGenresTable::TABLE_NAME,  MetaGenresTable::USER_ID_FULL,  $me->getId()));
+        $tracks  = count(new SelectQuery(MetadataTable::TABLE_NAME,    MetadataTable::USER_ID_FULL,    $me->getId()));
+
         $response->write([
             "email" => $me->getEmail(),
-            "id" => $me->getId()
+            "id" => $me->getId(),
+            "stats" => [
+                "artists_count" => $artists,
+                "albums_count" => $albums,
+                "genres_count" => $genres,
+                "tracks_count" => $tracks
+            ]
         ]);
 
     }
