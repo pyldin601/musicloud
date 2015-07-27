51532a66e3e2ba4792a96680152f37d85ce43272
pldin601 <roman@homefs.biz>
2015-07-27 21:48:43 +0300

    Artists view

--- app/project/handlers/dynamic/content/DoReadCover.php
+++ app/project/handlers/dynamic/content/DoReadCover.php
@@ -9,12 +9,25 @@
 namespace app\project\handlers\dynamic\content;
 
 
+use app\core\db\builder\SelectQuery;
+use app\core\exceptions\status\PageNotFoundException;
 use app\core\router\RouteHandler;
+use app\project\exceptions\BadAccessException;
+use app\project\models\single\LoggedIn;
 use app\project\models\tracklist\Track;
+use app\project\persistence\db\tables\MetadataTable;
+use app\project\persistence\fs\FileServer;
 
 class DoReadCover implements RouteHandler {
-    public function doGet($id) {
-        $tm = new Track($id);
-        $tm->cover();
+    public function doGet($id, LoggedIn $me) {
+
+        (new SelectQuery(MetadataTable::TABLE_NAME, MetadataTable::COVER_FILE_ID_FULL, $id))
+            ->fetchOneRow()
+            ->orThrow(PageNotFoundException::class)
+            ->filter(function ($row) use ($me) { return $row[MetadataTable::USER_ID] === $me->getId(); })
+            ->orThrow(BadAccessException::class);
+
+        FileServer::writeToClient($id);
+
     }
 }
