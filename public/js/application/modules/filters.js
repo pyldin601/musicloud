ad8fbe64236fddb07c17646deed7cad4ec8ad03a
pldin601 <roman@homefs.biz>
2015-07-29 12:13:28 +0300

    Working with multi selector

--- public/js/application/modules/filters.js
+++ public/js/application/modules/filters.js
@@ -61,13 +61,36 @@ homecloud.filter("getAlbumArtist", function () {
 });
 
 homecloud.filter("groupBy", function () {
+    var results={};
     return function (data, key) {
-        if (!data) return;
-        var result = {};
-        for (var i=0; i<data.length; i += 1) {
+        if (!(data && key)) return;
+        var result;
+        if(!this.$id){
+            result={};
+        }else{
+            var scopeId = this.$id;
+            if(!results[scopeId]){
+                results[scopeId]={};
+                this.$on("$destroy", function() {
+                    delete results[scopeId];
+                });
+            }
+            result = results[scopeId];
+        }
+
+        for(var groupKey in result) if (result.hasOwnProperty(groupKey))
+            result[groupKey].splice(0,result[groupKey].length);
+
+        for (var i=0; i<data.length; i++) {
             if (!result[data[i][key]])
                 result[data[i][key]]=[];
-            result[data[i][key]].push(data[i])
+            result[data[i][key]].push(data[i]);
+        }
+
+        var keys = Object.keys(result);
+        for(var k=0; k<keys.length; k++){
+            if(result[keys[k]].length===0)
+                delete result[keys[k]];
         }
         return result;
     };
