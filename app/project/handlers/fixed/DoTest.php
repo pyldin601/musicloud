74efaa9f826b1bc67163a8a4f106f2b780197233
pldin601 <roman@homefs.biz>
2015-08-05 20:51:02 +0300

    Album artist in albums view

--- app/project/handlers/fixed/DoTest.php
+++ app/project/handlers/fixed/DoTest.php
@@ -9,32 +9,18 @@
 namespace app\project\handlers\fixed;
 
 
-use app\core\db\builder\SelectQuery;
-use app\core\db\builder\UpdateQuery;
-use app\core\exceptions\ApplicationException;
-use app\core\http\HttpGet;
 use app\core\router\RouteHandler;
-use app\project\libs\FFProbe;
-use app\project\persistence\db\tables\TSongs;
-use app\project\persistence\fs\FileServer;
-use app\project\persistence\fs\FSTools;
 
 class DoTest implements RouteHandler {
-    public function doGet(HttpGet $httpGet) {
+    public function doGet() {
         header("Content-Type: text/plain");
-        (new SelectQuery(TSongs::_NAME))
-            ->where(TSongs::FORMAT . " IS NULL")
-            ->where(TSongs::FILE_ID . " IS NOT NULL")
-            ->eachRow(function ($row) {
-                $file_path = FileServer::getFileUsingId($row[TSongs::FILE_ID]);
-                $metadata = FFProbe::read($file_path);
-                if ($metadata->nonEmpty()) {
-                    $format_name = explode(",", $metadata->get()->format_name)[0];
-                    (new UpdateQuery(TSongs::_NAME, TSongs::ID, $row[TSongs::ID]))
-                        ->set(TSongs::FORMAT, $format_name)
-                        ->update();
-                }
-            });
+        $i = 10;
+        while ($i -- > 0) {
+            echo "Hello, World!" . PHP_EOL;
+            flush();
+            sleep(1);
+        }
+        echo "Done";
     }
 }
 
