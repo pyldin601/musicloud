ff8b99e8106d138e07ebf18461a2d1f568bc9f47
pldin601 <roman@homefs.biz>
2015-08-13 17:01:11 +0300

    Adding login form

--- public/js/application/lobby.js
+++ public/js/application/lobby.js
@@ -1,7 +1,37 @@
 (function () {
 
-    var lobby = angular.module("Lobby", []);
+    var lobby = angular.module("Lobby", ["httpPostFix"]);
 
+    lobby.controller("LoginController", ["$scope", "AccountService", "$location",
+        function ($scope, AccountService, $location) {
+            $scope.login = "";
+            $scope.password = "";
+            $scope.status = "";
+            $scope.submit = function () {
+                $scope.status = "";
+                AccountService.login({email: $scope.login, password: $scope.password})
+                    .success(function () {
+                        window.location.href = "/library/";
+                    })
+                    .error(function (error) {
+                        $scope.status = error.message;
+                    });
+            }
+        }
+    ]);
 
+    lobby.factory("AccountService", ["$http", function ($http) {
+        return {
+            login: function (data) {
+                return $http.post("/api/login", data);
+            },
+            logout: function () {
+                return $http.post("/api/logout");
+            },
+            init: function () {
+                return $http.get("/api/self");
+            }
+        };
+    }]);
 
 })();
