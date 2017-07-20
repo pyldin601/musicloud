912cb541f849d1efc4e0f3e4f692f12728a76c78
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-07-20 21:02:36 +0300

    Add reset.css and make gulp task for it

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 const gulp = require('gulp');
 const less = require('gulp-less');
 
-gulp.task('css', () =>
+gulp.task('less', () =>
     gulp
         .src('src/styles/*.less')
         .pipe(less())
         .pipe(gulp.dest('public/css'))
 );
 
-gulp.task('default', ['css']);
+gulp.task('css', () =>
+  gulp
+    .src('src/styles/*.css')
+    .pipe(gulp.dest('public/css'))
+);
+
+gulp.task('default', ['less', 'css']);
