8dda965e857601fc3ab5d9095683ce8a5b2e0784
Roman <roman.lakhtadyr@gmail.com>
2020-11-22 19:07:39 +0200

    feat: upload artwork on paste

--- src/scripts/musicloud/controllers/MetadataController.js
+++ src/scripts/musicloud/controllers/MetadataController.js
@@ -20,79 +20,108 @@
  * SOFTWARE.
  */
 
-import { first } from 'lodash';
+import { first } from 'lodash'
 
-export default ["$scope", "TrackService", "SyncService", "$filter", "$route",
-  ($scope, TrackService, SyncService, $filter, $route) => {
-
-    const songs = $scope.songs;
-    const artists_list = songs.map(field("album_artist")).distinct();
-    const cover_url = songs.map(field("middle_cover_id")).reduce(or);
-    const is_compilation = songs.all(field("is_compilation"));
-    const unmodified  = "[unmodified]";
+export default [
+  '$scope',
+  'TrackService',
+  'SyncService',
+  '$filter',
+  ($scope, TrackService, SyncService, $filter) => {
+    const songs = $scope.songs
+    const artists_list = songs.map((t) => t.album_artist).distinct()
+    const cover_url = songs.map((t) => t.middle_cover_id).reduce(or)
+    const is_compilation = songs.all((t) => t.is_compilation)
+    const unmodified = '[unmodified]'
 
     $scope.fields = {
-      track_title: "",
-      track_artist: "",
-      track_album: "",
-      album_artist: "",
-      track_number: "",
-      disc_number: "",
-      track_genre: "",
-      track_year: ""
-    };
+      track_title: '',
+      track_artist: '',
+      track_album: '',
+      album_artist: '',
+      track_number: '',
+      disc_number: '',
+      track_genre: '',
+      track_year: '',
+    }
 
     $scope.flags = {
-      is_compilation: is_compilation
-    };
+      is_compilation: is_compilation,
+    }
 
-    $scope.current_cover = cover_url;
+    $scope.current_cover = cover_url
 
     $scope.selected = {
-      artists: (artists_list.length === 1)
-        ? $filter("normalizeArtist")(first(artists_list))
-        : ` ${artists_list.length} artist(s)`,
-      songs: (songs.length === 1)
-        ? $filter("normalizeTrackTitle")(first(songs))
-        : "" + songs.length + " song(s)"
-    };
+      artists:
+        artists_list.length === 1
+          ? $filter('normalizeArtist')(first(artists_list))
+          : ` ${artists_list.length} artist(s)`,
+      songs:
+        songs.length === 1
+          ? $filter('normalizeTrackTitle')(first(songs))
+          : '' + songs.length + ' song(s)',
+    }
 
     $scope.load = function () {
       for (let i = 0; i < songs.length; i += 1) {
-        for (let key in $scope.fields) if ($scope.fields.hasOwnProperty(key)) {
-          if (i === 0) {
-            $scope.fields[key] = songs[0][key];
-          } else if (songs[0][key] !== songs[i][key]) {
-            $scope.fields[key] = unmodified;
+        for (let key in $scope.fields)
+          if ($scope.fields.hasOwnProperty(key)) {
+            if (i === 0) {
+              $scope.fields[key] = songs[0][key]
+            } else if (songs[0][key] !== songs[i][key]) {
+              $scope.fields[key] = unmodified
+            }
           }
-        }
       }
-    };
+    }
 
     $scope.save = function () {
-      const song_id = songs.map(function (e) {
+      const song_id = songs
+        .map(function (e) {
           return e.id
-        }).join(",");
-      const submission = { song_id: song_id, metadata: {} };
+        })
+        .join(',')
+      const submission = { song_id: song_id, metadata: {} }
 
-      for (let key in $scope.fields) if ($scope.fields.hasOwnProperty(key)) {
-        if ($scope.fields[key] !== unmodified) {
-          submission.metadata[key] = $scope.fields[key];
+      for (let key in $scope.fields)
+        if ($scope.fields.hasOwnProperty(key)) {
+          if ($scope.fields[key] !== unmodified) {
+            submission.metadata[key] = $scope.fields[key]
+          }
         }
-      }
 
       if ($scope.flags.is_compilation !== is_compilation) {
         submission.metadata.is_compilation = $scope.flags.is_compilation
       }
 
       TrackService.edit(submission).success(function (updated) {
-        SyncService.tracks(updated.tracks);
-        $scope.closeThisWindow();
-      });
+        SyncService.tracks(updated.tracks)
+        $scope.closeThisWindow()
+      })
+    }
 
-    };
+    $scope.paste = (event) => {
+      const trackIds = songs.map((e) => e.id).join(',')
+      const clipboardData = event.clipboardData ?? event.originalEvent.clipboardData
+      const items = clipboardData?.items ?? []
+      for (const item of items) {
+        if (item.kind !== 'file') continue
+        const file = item.getAsFile()
+        if (file.type.startsWith('image/')) {
+          // Upload cover
+          const form = new FormData()
+          form.append('artwork_file', file)
+          form.append('track_id', trackIds)
 
-    $scope.load();
+          TrackService.changeArtwork(form).success(function (updated) {
+            SyncService.tracks(updated)
+          })
+          event.preventDefault()
+          return
+        }
+      }
+    }
 
-  }
-];
+    $scope.load()
+  },
+]
