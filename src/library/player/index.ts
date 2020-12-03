44df9965339ea3c042c4235c9671ccb8e4da830f
Roman <roman.lakhtadyr@gmail.com>
2020-12-03 11:23:12 +0200

    feat: add mobx-angularjs dependency

--- src/library/player/index.ts
+++ src/library/player/index.ts
@@ -1,10 +1,9 @@
 import angular from 'angular'
+import mobxAngularjs from 'mobx-angularjs'
 import { PlayerService } from './PlayerService'
 
 const MODULE_NAME = 'MusicLoud.Player2'
 
-const player = angular.module(MODULE_NAME, [])
-
-player.service(PlayerService.name, PlayerService)
-
-export default MODULE_NAME
+export default angular
+  .module(MODULE_NAME, [mobxAngularjs])
+  .service(PlayerService.name, PlayerService)
