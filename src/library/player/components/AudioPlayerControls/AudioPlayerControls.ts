fcfbced0f786461acff24dba59c41ee57125d1c1
Roman <roman.lakhtadyr@gmail.com>
2020-12-04 19:00:24 +0200

    feat: connect new player to audio-player-controls component

--- src/library/player/components/AudioPlayerControls/AudioPlayerControls.ts
+++ src/library/player/components/AudioPlayerControls/AudioPlayerControls.ts
@@ -1,28 +1,44 @@
 import templateUrl from './AudioPlayerControls.html'
+import { AudioPlayerQueueService } from '../../services/AudioPlayerQueueService'
+import { computed, makeObservable } from 'mobx'
 
 const COMPONENT_NAME = 'audioPlayerControls'
 
 class AudioPlayerControlsController {
+  constructor(private readonly audioPlayerQueueService: AudioPlayerQueueService) {
+    makeObservable(this, {
+      isPlaying: computed,
+      isBuffering: computed,
+    })
+
+    Object.assign(window, { audioPlayerQueueService })
+  }
+
+  // [{ src: 'https://filesamples.com/samples/audio/mp3/sample2.mp3' }, { src: 'https://filesamples.com/samples/audio/mp3/sample2.mp3' }]
   public get isPlaying(): boolean {
-    return false
+    return this.audioPlayerQueueService.isPlaying
   }
 
   public get isBuffering(): boolean {
-    return false
+    return this.audioPlayerQueueService.isLoading
   }
 
   public playPrevious(): void {
-    //
+    this.audioPlayerQueueService.playPrevious()
   }
 
   public playNext(): void {
-    //
+    this.audioPlayerQueueService.playNext()
   }
 
   public playOrPause(): void {
-    //
+    if (this.audioPlayerQueueService.isPaused || this.audioPlayerQueueService.isPlaying) {
+      this.audioPlayerQueueService.playPause()
+    }
   }
 }
+
+AudioPlayerControlsController.$inject = [AudioPlayerQueueService.name]
 
 export const AudioPlayerControls = {
   [COMPONENT_NAME]: {
